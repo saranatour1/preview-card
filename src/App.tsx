e70f64f3677c03210b6e4d22c3de93e67097b507
saranatour1 <saranatourcontact@gmail.com>
2024-04-01 11:01:35 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Router, Route, Link } from "kaioken"
+import { Counter } from "./Counter"
+import '@fontsource-variable/figtree';
+
+export function App() {
+  return (
+  <main className="font-figtree flex flex-col items-center justify-center w-full h-full min-h-screen max-w-full bg-yellow ">
+    
+    <div className="w-full h-full max-w-[24rem] max-h-[32.625rem] p-6 rounded-[20px] bg-white shadow-3xl">
+fdgdfg
+    </div>
+  
+  </main>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        "yellow":"#F4D04E",
+        "grey":"hsl(0, 0%, 50%)",
+        "black-c":"hsl(0, 0%, 7%)",
+      },
+      fontFamily:{
+        figtree:['Figtree Variable', 'sans-serif']
+      },
+      boxShadow:{
+        '3xl':"8px 8px 0px #000000"
+      },
+      backgroundColor: {
+        primary: "rgb(220, 20, 60)",
+        "primary-light": "rgb(228 50 86)",
+      },
+    },
+  },
+  plugins: [],
+}
